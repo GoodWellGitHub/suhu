7764fe8686aecded3cb420c5a0febf3f496c7303
huajun.wang01 <2452255928@qq.com>
2018-10-28 00:41:13 +0800

    remote

--- provider/src/main/java/com/va/remote/UserInfoRemoteImpl.java
+++ provider/src/main/java/com/va/remote/UserInfoRemoteImpl.java
@@ -1,7 +1,18 @@
 package com.va.remote;
 
+import suloova.core.remote.UserInfoRemote;
+import suloova.model.ao.UserAo;
+import suloova.model.vo.UserVo;
+
 /**
  * Created by huajun.wang01 on 2018/10/21.
  */
 public class UserInfoRemoteImpl implements UserInfoRemote {
+    public UserAo queryUserInfo(UserVo userVo) {
+        return null;
+    }
+
+    public Integer saveUser(UserAo userAo) {
+        return null;
+    }
 }
